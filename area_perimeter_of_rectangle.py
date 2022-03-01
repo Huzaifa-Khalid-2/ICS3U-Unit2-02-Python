eed1dd43b415e2f28ab2d6e21d6230138734422e
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-03-01 19:14:51 +0000

    edited area_perimeter

--- area_perimeter_of_rectangle.py
+++ area_perimeter_of_rectangle.py
@@ -3,7 +3,7 @@
 # Created by: Huzaifa Khalid
 # Created on: February 2022
 # This program calculates the area and perimeter of a rectangle
-#    with integers inputted by user 
+#    with integers inputted by user
 
 import math
 
@@ -12,21 +12,23 @@ def main():
     # this function calculates the area and perimeter
 
     # input
+
     Length = int(input("Enter length of the rectangle (mm): "))
     Width = int(input("Enter width of the rectangle (mm): "))
-    
-    
+
     # process
-    
+
     area = Length * Width
     perimeter = 2 * (Length + Width)
-    
-    # output 
+
+    # output
+
     print("")
     print("Area is {0} mm².".format(area))
     print("Perimeter is {0} mm.".format(perimeter))
-    print("")
-    print("Done")
-    
+
+    print("\nDone")
+
+
 if __name__ == "__main__":
     main()
